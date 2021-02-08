189b7b99585f15ef7393c5b6fa5949a9f8401392
Bharathi <Bharathi@SNS-CPU-042>
2021-02-08 10:21:38 +0530

    Feb 8 - ConsoleUtilities

---
+++ gr_optimization/src/test/java/com/sns/gr_optimization/testbase/ConsoleUtilities.java
@@ -0,0 +1,26 @@
+package com.sns.gr_optimization.testbase;
+
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.util.logging.Level;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.logging.LogEntries;
+import org.openqa.selenium.logging.LogEntry;
+import org.openqa.selenium.logging.LogType;
+
+public class ConsoleUtilities {
+
+	public String analyzeLog(WebDriver driver, String page) {
+        LogEntries logEntries = driver.manage().logs().get(LogType.BROWSER);
+        SimpleDateFormat format = new SimpleDateFormat("YYYY-MM-dd HH:mm:ss");
+        String message = null;
+        for (LogEntry entry : logEntries) {
+        	if((entry.getLevel().equals(Level.SEVERE)) || (entry.getMessage().contains("404"))) {
+        		message = "Console Error in "+ page + " - " + format.format(new Date(entry.getTimestamp())) + " " + entry.getLevel() + " " + entry.getMessage();
+        		System.out.println(message);
+        	}      	
+        }
+        return message;
+    }
+}
