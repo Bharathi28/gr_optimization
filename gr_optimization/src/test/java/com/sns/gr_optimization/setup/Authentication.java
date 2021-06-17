257a01409aa4903741f3dabf1d02cc2dcf21fa9e
DESKTOP-DLQJIDD\happy <manibharathi@searchnscore.com>
2021-06-17 16:25:00 +0530

    June 17 - update

---
+++ gr_optimization/src/test/java/com/sns/gr_optimization/setup/Authentication.java
@@ -0,0 +1,22 @@
+package com.sns.gr_optimization.setup;
+
+import static io.restassured.RestAssured.given;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import io.restassured.path.json.JsonPath;
+
+public class Authentication {
+	
+	public void SharepointAuthentication() {
+		
+	}
+	
+	public void sharepointFileDownload(String access_token, String sp_folder_name, String sp_file_name, String download_path) throws IOException {
+						
+			
+	}
+}
